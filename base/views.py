839c57b0d787294fccf0908a7b33ca98cc2d88e8
Kutuwa <kutuwaruksh.55@gmail.com>
2022-07-25 15:58:39 +0545

    commit

---
+++ base/views.py
@@ -0,0 +1,212 @@
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView, View, CreateView, FormView
+from .models import *
+from .forms import *
+from django.urls import reverse_lazy 
+from django.contrib.auth import login, logout, authenticate
+
+# Create your views here.
+class HomeView(TemplateView):
+    template_name = "base/home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product_list'] = Product.objects.all()
+        return context
+
+#allproduct views show the all the product differetiate in different categories
+class AllProductsView(TemplateView):
+    template_name = "base/allproducts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['allcategories'] = Category.objects.all()
+        return context
+
+#detail of the product
+class ProductDetailView(TemplateView):
+    template_name = "base/productdetail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        product = Product.objects.get(slug=url_slug)#here .get retrives the single data of the product from the database
+        product.view_count += 1
+        product.save()
+        context['product'] = product
+        return context
+
+class AddToCartView(TemplateView):
+    template_name = "base/addtocart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # get product id from requested url
+        product_id = self.kwargs['pro_id']
+        # get product
+        product_obj = Product.objects.get(id=product_id)
+
+        # check if cart exists
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            this_product_in_cart = cart_obj.cartproduct_set.filter(
+                product=product_obj)#we get all cart product in cart
+
+            # item already exists in cart
+            if this_product_in_cart.exists():
+                cartproduct = this_product_in_cart.last()
+                cartproduct.quantity += 1
+                cartproduct.subtotal += product_obj.selling_price
+                cartproduct.save()
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+            # new item is added in cart
+            else:
+                cartproduct = CartProduct.objects.create(
+                    cart=cart_obj, product=product_obj, rate=product_obj.selling_price, quantity=1, subtotal=product_obj.selling_price)
+                cart_obj.total += product_obj.selling_price
+                cart_obj.save()
+
+        else:
+            cart_obj = Cart.objects.create(total=0)
+            self.request.session['cart_id'] = cart_obj.id
+            cartproduct = CartProduct.objects.create(
+                cart=cart_obj, product=product_obj, rate=product_obj.selling_price, quantity=1, subtotal=product_obj.selling_price)
+            cart_obj.total += product_obj.selling_price
+            cart_obj.save()
+
+        return context
+
+class MyCartView(TemplateView):
+    template_name = "base/mycart.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)# we are using cart_id as session key
+        if cart_id:
+            cart = Cart.objects.get(id=cart_id)
+        else:
+            cart = None
+        context['cart'] = cart
+        return context
+
+
+#managecartview is to increase, decrease, and remove product directly form th efront end 
+class ManageCartView(View):
+    def get(self, request, *args, **kwargs):
+        cp_id = self.kwargs["cp_id"]#to send or get dynamic id form url we need to use self.kwargs
+        action = request.GET.get("action")
+        cp_obj = CartProduct.objects.get(id=cp_id)
+        cart_obj = cp_obj.cart
+
+        if action == "inc":
+            cp_obj.quantity += 1
+            cp_obj.subtotal += cp_obj.rate
+            cp_obj.save()
+            cart_obj.total += cp_obj.rate
+            cart_obj.save()
+        elif action == "dcr":
+            cp_obj.quantity -= 1
+            cp_obj.subtotal -= cp_obj.rate
+            cp_obj.save()
+            cart_obj.total -= cp_obj.rate
+            cart_obj.save()
+            if cp_obj.quantity == 0:
+                cp_obj.delete()
+
+        elif action == "rmv":
+            cart_obj.total -= cp_obj.subtotal
+            cart_obj.save()
+            cp_obj.delete()
+        else:
+            pass
+        return redirect("mycart")
+        
+
+class CheckoutView(CreateView):
+    template_name = "base/checkout.html"
+    form_class = CheckoutForm
+    success_url = reverse_lazy("home")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cart_id = self.request.session.get("cart_id", None)
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+        else:
+            cart_obj = None
+        context['cart'] = cart_obj
+        return context
+
+    def form_valid(self, form):
+        cart_id = self.request.session.get("cart_id")
+        if cart_id:
+            cart_obj = Cart.objects.get(id=cart_id)
+            form.instance.cart = cart_obj
+            form.instance.subtotal = cart_obj.total
+            form.instance.discount = 0
+            form.instance.total = cart_obj.total
+            form.instance.order_status = "Order Received"
+            del self.request.session['cart_id']#product in cart is deleted after placing order
+            pm = form.cleaned_data.get("payment_method")
+            order = form.save()
+            if pm == "Khalti":
+                return redirect(reverse("ecomapp:khaltirequest") + "?o_id=" + str(order.id))
+            elif pm == "Esewa":
+                return redirect(reverse("ecomapp:esewarequest") + "?o_id=" + str(order.id))
+        else:
+            return redirect("ecomapp:home")
+        return super().form_valid(form)
+
+class CustomerRegistrationView(CreateView):
+    template_name = "base/customerregistration.html"
+    form_class = CustomerRegistrationForm
+    success_url = reverse_lazy("home")
+
+    def form_valid(self, form):
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        email = form.cleaned_data.get("email")
+        user = User.objects.create_user(username, email, password)
+        form.instance.user = user
+        login(self.request, user)
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        if "next" in self.request.GET:
+            next_url = self.request.GET.get("next")
+            return next_url
+        else:
+            return self.success_url
+
+
+class CustomerLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect("home")
+
+    
+class CustomerLoginView(FormView):
+    template_name = "base/customerlogin.html"
+    form_class = CustomerLoginForm
+    success_url = reverse_lazy("home")
+
+    # form_valid method is a type of post method and is available in createview formview and updateview
+    def form_valid(self, form):
+        uname = form.cleaned_data.get("username")
+        pword = form.cleaned_data["password"]
+        usr = authenticate(username=uname, password=pword)
+        if usr is not None and Customer.objects.filter(user=usr).exists():
+            login(self.request, usr)
+        else:
+            return render(self.request, self.template_name, {"form": self.form_class, "error": "Invalid credentials"})
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        if "next" in self.request.GET:
+            next_url = self.request.GET.get("next")
+            return next_url
+        else:
+            return self.success_url
